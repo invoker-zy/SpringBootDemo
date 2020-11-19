abf3fa3ba47a7c4ef1cdd456950daf6881c978c0
invoker-zy <75025477@qq.com>
2020-11-19 17:30:48 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import com.example.demo.controller.HelloController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @SpringBootApplication 来标注一个程序类，说明这是一个 Spring Boot 应用
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        //启动Spring应用
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
