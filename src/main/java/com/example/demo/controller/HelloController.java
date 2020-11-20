a2a222ad6dc95a2aade6b31a646a41eea7d955ca
invoker-zy <75025477@qq.com>
2020-11-20 15:59:57 +0800

    yml配置文件

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -4,11 +4,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration
-@Controller
+//@ResponseBody
+//@Controller
+@RestController
 public class HelloController {
-    @ResponseBody
+
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World!";
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Spring Boot单元测试
+ * 可以在测试期间很方便的类似编码一样进行自动注入等容器的功能
+ */
 @SpringBootTest
 class DemoApplicationTests {
-
+    @Autowired
+    Person person;
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
