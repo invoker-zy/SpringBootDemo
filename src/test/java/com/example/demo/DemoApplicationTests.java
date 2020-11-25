54c413a169434f2875852e94bee2ce184c69e21d
invoker-zy <75025477@qq.com>
2020-11-25 18:02:00 +0800

    Spring配置

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import com.example.demo.bean.Person;
 import com.example.demo.service.HelloService;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -13,10 +16,12 @@ import org.springframework.context.ApplicationContext;
  */
 @SpringBootTest
 class DemoApplicationTests {
+
     @Autowired
     Person person;
     @Autowired
     ApplicationContext ioc;
+    Logger logger = LoggerFactory.getLogger(getClass());
     @Test
     public void testHelloService(){
         boolean b = ioc.containsBean("helloService");
@@ -24,6 +29,15 @@ class DemoApplicationTests {
     }
     @Test
     void contextLoads() {
+
+        //日志的级别
+        //由低到高 trace debug info warn error
+        logger.trace("这是一条跟踪日志");
+        logger.debug("这是一条调试日志");
+        //Spring Boot默认使用的是info级别的,Root级别
+        logger.info("这是一条普通日志");
+        logger.warn("这是一条警告日志");
+        logger.error("这是一条错误日志");
         System.out.println(person);
     }
 
